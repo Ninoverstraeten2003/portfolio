1e0c8921de5a0973ac7fa3d4a32173aebac2770a
Nino Verstraeten <r0898606@ucll.be>
2024-09-01 21:35:30 +0200

    test

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -31,15 +31,23 @@ export default function RootLayout({
 					strategy="afterInteractive"
 					dangerouslySetInnerHTML={{
 						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TVQMDCWC');`,
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','GTM-TPWMJFWW');`,
 					}}
 				></Script>
 				<script defer src="script.js"></script>
 			</head>
 			<body className={`${geistSans.variable} ${geistMono.variable} dark`}>
+				<noscript>
+					<iframe
+						src="https://www.googletagmanager.com/ns.html?id=GTM-TPWMJFWW"
+						height="0"
+						width="0"
+						className="hidden"
+					></iframe>
+				</noscript>
 				<Providers>{children}</Providers>
 			</body>
 		</html>
