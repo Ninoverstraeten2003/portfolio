5c1c45f1bc2fbcaf151e160049ccba4fc9617e65
Nino Verstraeten <r0898606@ucll.be>
2024-09-01 20:44:52 +0200

    add script

--- portfolio/src/app/layout.tsx
+++ portfolio/src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
 import Providers from "./providers";
+import Script from "next/script";
 
 const geistSans = localFont({
 	src: "./fonts/GeistVF.woff",
@@ -24,6 +25,20 @@ export default function RootLayout({
 }>) {
 	return (
 		<html lang="en">
+			<head>
+				<Script
+					id="gtm"
+					strategy="afterInteractive"
+					dangerouslySetInnerHTML={{
+						__html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TVQMDCWC');`,
+					}}
+				></Script>
+				<script defer src="script.js"></script>
+			</head>
 			<body className={`${geistSans.variable} ${geistMono.variable} dark`}>
 				<Providers>{children}</Providers>
 			</body>
--- portfolio/src/app/links.tsx
+++ portfolio/src/app/links.tsx
@@ -2,66 +2,48 @@
 
 import { Button } from "@/components/ui/button";
 import { Separator } from "@/components/ui/separator";
-import useHash from "@/hooks/use-hash";
 import Link from "next/link";
-import { useParams } from "next/navigation";
-import { useEffect } from "react";
 
 export function Links() {
-	const params = useParams();
-	const hash = useHash();
-
 	return (
 		<ul className="space-y-2">
-			<li>
-				<a className="group flex items-center py-2" href="#about">
+			<li className="group about">
+				<a className="flex items-center py-2 " href="#about">
 					<span
-						className={`nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 ${
-							hash === "about" ? "active" : ""
-						} [&.active]:bg-slate-200 [&.active]:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none`}
+						className={`nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-[.active]:bg-slate-200 group-[.active]:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none`}
 					></span>
 					<span
-						className={` ${
-							hash === "about" ? "active" : ""
-						} nav-text text-xs font-bold uppercase tracking-widest text-muted-foreground [&.active]:text-slate-200 group-hover:text-slate-200 group-focus-visible:text-slate-200`}
+						className={`nav-text text-xs font-bold uppercase tracking-widest text-muted-foreground group-[.active]:text-slate-200 group-hover:text-slate-200 group-focus-visible:text-slate-200`}
 					>
 						About
 					</span>
 				</a>
 			</li>
-			<li>
-				<a className="group flex items-center py-2" href="#experience">
+			<li className="group experience">
+				<a className="flex items-center py-2 " href="#experience">
 					<span
-						className={`nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 ${
-							hash === "experience" ? "active" : ""
-						} [&.active]:bg-slate-200 [&.active]:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none`}
+						className={`nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-[.active]:bg-slate-200 group-[.active]:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none`}
 					></span>
 					<span
-						className={` ${
-							hash === "experience" ? "active" : ""
-						} nav-text text-xs font-bold uppercase tracking-widest text-muted-foreground [&.active]:text-slate-200 group-hover:text-slate-200 group-focus-visible:text-slate-200`}
+						className={`nav-text text-xs font-bold uppercase tracking-widest text-muted-foreground group-[.active]:text-slate-200 group-hover:text-slate-200 group-focus-visible:text-slate-200`}
 					>
 						Experience
 					</span>
 				</a>
 			</li>
-			<li>
-				<a className="group flex items-center py-2" href="#projects">
+			<li className="group projects">
+				<a className="flex items-center py-2 " href="#projects">
 					<span
-						className={`nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 ${
-							hash === "projects" ? "active" : ""
-						} [&.active]:bg-slate-200 [&.active]:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none`}
+						className={`nav-indicator mr-4 h-px w-8 bg-slate-600 transition-all group-hover:w-16 group-[.active]:bg-slate-200 group-[.active]:w-16 group-hover:bg-slate-200 group-focus-visible:w-16 group-focus-visible:bg-slate-200 motion-reduce:transition-none`}
 					></span>
 					<span
-						className={` ${
-							hash === "projects" ? "active" : ""
-						} nav-text text-xs font-bold uppercase tracking-widest text-muted-foreground [&.active]:text-slate-200 group-hover:text-slate-200 group-focus-visible:text-slate-200`}
+						className={`nav-text text-xs font-bold uppercase tracking-widest text-muted-foreground group-[.active]:text-slate-200 group-hover:text-slate-200 group-focus-visible:text-slate-200`}
 					>
 						Projects
 					</span>
 				</a>
 			</li>
-			<Separator className="w-11/12"/>
+			<Separator className="w-11/12" />
 			<li className="group flex items-center py-2">
 				<Button asChild variant="link" className="p-0">
 					<Link
