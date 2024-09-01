e19e708f79a6161bfab735ab6a94aac90275bc41
Nino Verstraeten <r0898606@ucll.be>
2024-09-01 12:01:44 +0200

    fix

--- portfolio/src/app/project.tsx
+++ portfolio/src/app/project.tsx
@@ -10,53 +10,6 @@ export interface ProjectProps {
 	skills: string[];
 }
 
-interface ImageDetails {
-	id: string;
-	slug: string;
-	alternative_slugs: {
-		en: string;
-		es: string;
-		ja: string;
-		fr: string;
-		it: string;
-		ko: string;
-		de: string;
-		pt: string;
-	};
-	created_at: string;
-	updated_at: string;
-	promoted_at: string;
-	width: number;
-	height: number;
-	color: string;
-	blur_hash: string;
-	description: string | null;
-	alt_description: string;
-	breadcrumbs: Array<any>;
-	urls: {
-		raw: string;
-		full: string;
-		regular: string;
-		small: string;
-		thumb: string;
-		small_s3: string;
-	};
-	links: {
-		self: string;
-		html: string;
-		download: string;
-		download_location: string;
-	};
-	likes: number;
-	liked_by_user: boolean;
-	current_user_collections: Array<any>;
-	sponsorship: any | null;
-	topic_submissions: {
-		[key: string]: any;
-	};
-	asset_type: string;
-}
-
 export default function Project({
 	title,
 	description,
@@ -66,7 +19,7 @@ export default function Project({
 	const {
 		data,
 		isLoading,
-	}: { data: { results: ImageDetails[] }; isLoading: boolean } =
+	} =
 		useGetPhotosByQuery({ query: description });
 
 	return (
